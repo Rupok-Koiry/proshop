b185be6e40a983503ce3a1cfc5880cd84dd40c0c
Rupok-Koiry <koiry.rupok@gmail.com>
2021-12-19 15:46:16 +0600

    first commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class="copy-right py-3 bg-success text-white">
+        <div class="container">
+          <p class="text-center m-0">
+            © 2021 <span class="logoName">PROSHOP</span>. All rights reserved |
+            Design by<span> Rupok Koiry.</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
